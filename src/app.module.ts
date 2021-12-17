f434adb3fc526d623808b01e6083b2be03bed259
Praxis <hello@vulpinecat.com>
2021-12-17 16:52:27 +0100

    hook up sqlite via typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: '../db/app.db',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { existsSync, writeFileSync } from 'fs';
 
 async function bootstrap() {
+  // ensure that the database exists
+  const dbPath = join(__dirname, '..', 'db', 'app.db');
+  if (!existsSync(dbPath)) {
+    writeFileSync(dbPath, '');
+  }
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
