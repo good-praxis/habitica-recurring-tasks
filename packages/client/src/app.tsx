1a58557027e576d047f2ea2b5fe1b025219b21b2
Praxis <hello@vulpinecat.com>
2022-02-21 16:32:48 +0100

    chore[Client]: switch to preact

---
+++ packages/client/src/app.tsx
@@ -0,0 +1,13 @@
+import { HrtTitle } from '@/components/HrtTitle';
+import { LoginForm } from '@/components/LoginForm';
+
+export function App() {
+  return (
+    <>
+      <div class="container mx-auto my-3">
+        <HrtTitle />
+        <LoginForm />
+      </div>
+    </>
+  );
+}
---
+++ packages/client/src/components/HrtTitle.tsx
@@ -0,0 +1,17 @@
+export function HrtTitle() {
+  return (
+    <>
+      <div class="container mx-auto font-serif text-center bg-transparent">
+        <h1 class="text-5xl text-blue-600 bg-transparent">
+          <span class="text-purple-600">Habitica</span>
+          <span class="text-2xl text-orange-600 align-super">*</span>
+          Recurring Tasks
+        </h1>
+        <h2 class="text-xl italic">
+          A <span class="text-orange-600">third-party app</span> for fully
+          featured recurring tasks.
+        </h2>
+      </div>
+    </>
+  );
+}
